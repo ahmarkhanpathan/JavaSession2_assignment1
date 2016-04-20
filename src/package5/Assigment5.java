33e64d804716e89b369a6cacca31aa2840a56ba1
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-04-20 11:34:59 +0530

    JavaSession2_Assignment1

---
+++ src/package5/Assigment5.java
@@ -0,0 +1,25 @@
+package package5;
+
+import java.util.Scanner;
+
+public class Assigment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner ab=new Scanner(System.in);
+		int age;
+		System.out.print("Please enter your age-");
+		
+		age = ab.nextInt();
+	if (age >=18)
+		
+		System.out.println("You Are eligible to Vote");
+		
+	else
+	    System.out.println("You are not eligible to Vote");
+			
+				
+	}
+
+}
